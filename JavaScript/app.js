c93e80faa35a38a2a31b48cb8d96ff893227c6b8
HrituRaz33 <95464215+HrituRaz33@users.noreply.github.com>
2022-03-07 04:25:09 +0600

    error 3 problem solve

--- JavaScript/app.js
+++ JavaScript/app.js
@@ -1,37 +1,62 @@
 const searchFood=()=>{
     const searchField =document.getElementById('search-field');
-    const searchText = searchField.value.trim();
-    searchField.innerHTML='';
+    const searchText = searchField.value;
+    // const searchResult= document.getElementById('search-result');
+    
+    
+    if(searchField.value == ''){
+      const bottomSrarchResult = document.getElementById('bottom-search-result');
+      const div = document.createElement('div');
+        div.innerHTML=`
+        <p style="color: orangered;" class="fw-bold text-center">Sorry!! did't find any meal</p>
+        `;
+        // console.log('Sorry!! We did not find');
+        bottomSrarchResult.innerHTML='';
+        bottomSrarchResult.appendChild(div);
+        const searchResult= document.getElementById('search-result');
+      searchResult.innerHTML='';
+        
+    }
     // 
-    const url =`https://www.themealdb.com/api/json/v1/1/search.php?s=${searchText}`
-    fetch(url)
-    .then(res => res.json())
-    .then(data =>displaySearchResult(data.meals));
+    else{
+      const url =`https://www.themealdb.com/api/json/v1/1/search.php?s=${searchText}`
+      fetch(url)
+      .then(res => res.json())
+      .then(data =>displaySearchResult(data.meals));
+    }
 }
 
 const displaySearchResult =(meals)=>{
-    // console.log(meals)
+    // console.log(meals) 
     const searchResult= document.getElementById('search-result');
+    const mealDetail = document.getElementById('p').style.display='none';
+    mealDetail.innerHTML='';
     // searchResult.innerHTML='';
     if(meals == null){
-        // console.log(' We did not find');
+      const bottomSrarchResult = document.getElementById('bottom-search-result');
+      bottomSrarchResult.innerHTML='';
         const div = document.createElement('div');
         div.innerHTML=`
         <p style="color: orangered;" class="fw-bold text-center">Sorry!! did't find any meal</p>
         `;
         // console.log('Sorry!! We did not find');
+        searchResult.innerHTML='';
         searchResult.appendChild(div);
     }
     else{
+      const searchResult= document.getElementById('search-result');
+      const bottomSrarchResult = document.getElementById('bottom-search-result');
+      bottomSrarchResult.innerHTML='';
+      searchResult.innerHTML='';
         meals.forEach(meal=>{
             // console.log(meals);
             const div = document.createElement('div');
             div.classList.add('col');
             div.innerHTML=`
             <div class="card mb-3">
-              <img src="${meal.strMealThumb}" class="card-img-top" alt="...">
+              <img src="${meal.strMealThumb}" class="card-img-top text-warning" alt="...">
                 <p class="fw-bold text-center">${meal.strMeal}</p>
-                <button id="recepi-button" onclick="loadMealDetail(${meal.idMeal})" class="btn-change4 rounded-3">Button</button>
+                <button id="recepi-button" onclick="loadMealDetail('${meal.idMeal}')" class="btn-change4 rounded-3">Button</button>
             </div>
             `;
             searchResult.appendChild(div);
@@ -40,6 +65,8 @@ const displaySearchResult =(meals)=>{
 }
 
 const loadMealDetail= (mealId) =>{
+  const mealDetail = document.getElementById('p').style.display='block';
+  const searchResult= document.getElementById('search-result').style.display='none';
     // console.log(mealId);
     const url =`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${mealId}`;
     fetch(url)
@@ -51,13 +78,13 @@ const loadMealDetail= (mealId) =>{
     const displayMealDetails= (meal) =>{
         // console.log(meal);
         const mealDetail = document.getElementById('p');
-        console.log(meal.strCategory);
+        console.log(meal.strInstructions);
         const div = document.createElement('div');
         div.classList.add('card');
         div.innerHTML=`
         <div class="text-center">
         <button type="button" class="close-button mt-2">
-          <i class="fa-solid fa-circle-xmark"></i>
+          <i id="cross-button" onclick="crossButton() class="fa-solid fa-circle-xmark"></i>
         </button>
     </div>
     <div class="text-center w-100 mx-auto">
@@ -74,4 +101,8 @@ const loadMealDetail= (mealId) =>{
    </div>
         `;
         mealDetail.appendChild(div);
+    }
+
+    const crossButton =()=>{
+       
     }
